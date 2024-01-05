0508e8d13711af43efa16bcd56cf790c45bec108
Vlad <vlad.pavelescu@gmail.com>
2024-01-05 12:59:02 +0000

    Added login, lobby, game and chat functionality

--- JS ClientServer/app.js
+++ JS ClientServer/app.js
@@ -8,6 +8,13 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+let players = new Map();
+let playersToSockets = new Map();
+let socketsToPlayers = new Map();
+let nextPlayerNumber = 0;
+let state = { state: 0, countdown: 5 };
+let timer = null;
+
 //Setup static page handling
 app.set('view engine', 'ejs');
 app.use('/static', express.static('public'));
@@ -32,10 +39,140 @@ function startServer() {
     });
 }
 
+//Handle errors
+function error(socket, message, halt) {
+  console.log('Error: ' + message);
+  socket.emit('fail',message);
+  if(halt) {
+      socket.disconnect();
+  }
+}
+
+//Handle announcements
+function announce(message) {
+  console.log('Announcement: ' + message);
+  io.emit('chat',message);
+}
+
+//Handle join
+function handleJoin(socket, username) {
+    //Can only join the game before it has started
+    if(state.state > 0) {
+        error(socket,'The game has already started',true);
+        return;
+    }
+
+    //Start new player
+    nextPlayerNumber++;
+    console.log('Welcome to player ' + nextPlayerNumber);
+
+    players.set(nextPlayerNumber,{ name: nextPlayerNumber, username: username, state: 1, score: 0 });
+    playersToSockets.set(nextPlayerNumber,socket);
+    socketsToPlayers.set(socket,nextPlayerNumber);
+}
+
+//Handle login
+function handleLogin(credentials, socket) {
+    let username = credentials.username;
+    let password = credentials.password;
+    //TODO: check credentials against backend
+
+    //TODO: if valid: 
+    handleJoin(socket, username)
+    updateAll()
+
+}
+
+//Handle registration
+function handleRegister(credentials, socket) {
+    let username = credentials.username;
+    let password = credentials.password;
+    //TODO: check credentials against backend
+
+    //TODO: if valid: 
+    handleJoin(socket, username)
+    updateAll()
+}
+
+//Update state of all players
+function updateAll() {
+  console.log('Updating all players');
+  for(let [playerNumber,socket] of playersToSockets) {
+      updatePlayer(socket);
+  }
+}
+
+//Update one player
+function updatePlayer(socket) {
+  const playerNumber = socketsToPlayers.get(socket);
+  const thePlayer = players.get(playerNumber);
+  const data = { state: state, me: thePlayer, players: Object.fromEntries(players) }; 
+  socket.emit('state', data);
+}
+
+//Handle admin actions
+function handleAdmin(player,action) {
+  if(player != 1) {
+      console.log('Failed admin action from player ' + player + ' for ' + action);
+      return;
+  }
+
+  if(action == 'start' && state.state == 0) {
+      startGame();
+  } else {
+      console.log('Unknown admin action: ' + action); 
+  }
+}
+
+//Start the game 
+function startGame() {
+  console.log('Game starting');
+  announce('Let the games begin');
+
+  //Prepare all players    
+  for(const [playerNumber,player] of players) {
+      player.state = 1;
+  }
+
+  //Start the timer
+  console.log('Starting the timer: ' + state.countdown);
+  timer = setInterval(() => {
+      tickGame();
+  }, 1000);
+
+  //Advance the game
+  state.state = 1;
+
+}
+
+//Game tick
+function tickGame() {
+  if(state.countdown > 1) {
+      state.countdown--;
+      console.log('Tick ' + state.countdown);
+  } else {
+      clearInterval(timer);
+      timer = null;
+      endGame();
+  }
+  updateAll();
+}
+
+//End game -> 2
+function endGame() {
+  state.state = 2;
+  console.log('Game ending');
+  for(let [playerNumber,player] of players) {
+      if(player.score < 100) {
+          console.log('Time up for ' + player.username);
+      }
+  }
+}
+
 //Chat message
-function handleChat(message) {
-    console.log('Handling chat: ' + message); 
-    io.emit('chat',message);
+function handleChat(player,message) {
+  console.log('Handling chat: ' + message + ' from player ' + player);
+  announce(player + ': ' + message);
 }
 
 //Handle new connection
@@ -44,12 +181,32 @@ io.on('connection', socket => {
 
   //Handle on chat message received
   socket.on('chat', message => {
-    handleChat(message);
+    if(!socketsToPlayers.has(socket)) return;
+    let player = socketsToPlayers.get(socket);
+    let thePlayer = players.get(player);
+    handleChat(thePlayer.username,message);
+  });
+
+  //Handle admin command
+  socket.on('admin', action => {
+    if(!socketsToPlayers.has(socket)) return;
+    handleAdmin(socketsToPlayers.get(socket),action);
+    updateAll();
   });
 
   //Handle disconnection
   socket.on('disconnect', () => {
     console.log('Dropped connection');
+  });
+
+  //Handle login
+  socket.on('login', credentials => {
+    handleLogin(credentials, socket);
+  });
+
+  //Handle registration
+  socket.on('register', credentials => {
+    handleRegister(credentials, socket);
   });
 });
 
--- JS ClientServer/public/game.js
+++ JS ClientServer/public/game.js
@@ -5,26 +5,54 @@ var app = new Vue({
   el: "#game",
   data: {
     connected: false,
+    error: null,
     messages: [],
     chatmessage: "",
-    signIn: false,
+    me: { name: '', username: '', state: 0, score: 0 },
+    state: { state: false },
+    players: {},
+    username: "",
+    password: "",
+    prompt: ""
   },
   mounted: function () {
     connect();
   },
   methods: {
+    admin(command) {
+      socket.emit('admin',command)
+    },
     handleChat(message) {
-      if (this.messages.length + 1 > 10) {
-        this.messages.pop();
-      }
-      this.messages.unshift(message);
+      const messages = document.getElementById('messages');
+      var item = document.createElement('li');
+      item.textContent = message;
+      messages.prepend(item);
+    },
+    fail(message) {
+      this.error = message;
+      setTimeout(clearError, 3000);
     },
     chat() {
       socket.emit("chat", this.chatmessage);
       this.chatmessage = "";
     },
+    login() {
+      socket.emit("login", {username: this.username, password: this.password});
+    },
+    register() {
+      socket.emit("register", {username: this.username, password: this.password});
+    },
+    update(data) {
+      this.me = data.me;
+      this.state = data.state;
+      this.players = data.players;
+    }
   },
 });
+
+function clearError() {
+  app.error = null;
+}
 
 function connect() {
   //Prepare web socket
@@ -47,8 +75,18 @@ function connect() {
     app.connected = false;
   });
 
+  //Handle error
+  socket.on("fail", function (message) {
+    app.fail(message);
+  });
+
   //Handle incoming chat message
   socket.on("chat", function (message) {
     app.handleChat(message);
+  });
+
+  //Handle state
+  socket.on("state", function (state) {
+    app.update(state);
   });
 }
