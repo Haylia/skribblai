4957554d741687ff413940424fbeeb89b8b8e23d
Vlad <vlad.pavelescu@gmail.com>
2024-01-09 17:09:38 +0000

    Added canvas

--- JS ClientServer/app.js
+++ JS ClientServer/app.js
@@ -12,7 +12,7 @@ let players = new Map();
 let playersToSockets = new Map();
 let socketsToPlayers = new Map();
 let nextPlayerNumber = 0;
-let state = { state: 0, countdown: 5 };
+let state = { state: 0, countdown: 90 };
 let timer = null;
 
 //Setup static page handling
@@ -108,26 +108,29 @@ function handleLogin(credentials, socket) {
   let username = credentials.username;
   let password = credentials.password;
 
-  // Prepare user information for login
-  const userInfo = { username, password };
-  const config = createRequestConfig("login", "/player/login", userInfo);
+  handleJoin(socket, username);
+  updateAll()
 
-  sendLogin(config)
-    .then((response) => {
-      const { result, msg } = response.data;
-      if (result) {
-        // If login is successful
-        handleJoin(socket, username);
-        updateAll();
-        socket.emit("login", { result, message: msg });
-      } else {
-        socket.emit("login", { result, message: msg });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      socket.emit("login", false, "Error during login");
-    });
+  // // Prepare user information for login
+  // const userInfo = { username, password };
+  // const config = createRequestConfig("login", "/player/login", userInfo);
+
+  // sendLogin(config)
+  //   .then((response) => {
+  //     const { result, msg } = response.data;
+  //     if (result) {
+  //       // If login is successful
+  //       handleJoin(socket, username);
+  //       updateAll();
+  //       socket.emit("login", { result, message: msg });
+  //     } else {
+  //       socket.emit("login", { result, message: msg });
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //     socket.emit("login", false, "Error during login");
+  //   });
 }
 
 function sendLogin(config) {
@@ -151,24 +154,27 @@ function handleRegister(credentials, socket) {
   let username = credentials.username;
   let password = credentials.password;
 
-  const userInfo = { username, password };
+  handleJoin(socket, username);
+  updateAll();
 
-  sendRegister("/player/register", userInfo)
-    .then((response) => {
-      const { result, msg } = response.data;
-      if (result) {
-        // If registration is successful
-        handleJoin(socket, username);
-        updateAll();
-        socket.emit("register", { result, message: msg });
-      } else {
-        socket.emit("register", { result, message: msg });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      socket.emit("register", false, "Error during registration");
-    });
+  // const userInfo = { username, password };
+
+  // sendRegister("/player/register", userInfo)
+  //   .then((response) => {
+  //     const { result, msg } = response.data;
+  //     if (result) {
+  //       // If registration is successful
+  //       handleJoin(socket, username);
+  //       updateAll();
+  //       socket.emit("register", { result, message: msg });
+  //     } else {
+  //       socket.emit("register", { result, message: msg });
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //     socket.emit("register", false, "Error during registration");
+  //   });
 }
 
 function sendRegister(url, userInfo) {
--- JS ClientServer/public/game.js
+++ JS ClientServer/public/game.js
@@ -5,6 +5,7 @@ var app = new Vue({
   el: "#game",
   data: {
     connected: false,
+    canvas_setup: false,
     error: null,
     messages: [],
     chatmessage: "",
@@ -52,9 +53,77 @@ var app = new Vue({
       this.me = data.me;
       this.state = data.state;
       this.players = data.players;
+      if(!this.canvas_setup) {
+        try{
+          setup_canvas();
+          this.canvas_setup = true;
+        } catch (e) {
+          console.log(e);
+        }
+      }
     },
   },
 });
+
+function setup_canvas() {
+  const canvas = document.getElementById('drawing-board');
+  const toolbar = document.getElementById('toolbar');
+  const ctx = canvas.getContext('2d');
+
+  const canvasOffsetX = canvas.offsetLeft;
+  const canvasOffsetY = canvas.offsetTop;
+
+  canvas.width = window.innerWidth / 2; // - (canvasOffsetX);
+  canvas.height = window.innerHeight / 2; //- (canvasOffsetY);
+
+  let isPainting = false;
+  let lineWidth = 5;
+  let startX;
+  let startY;
+
+  toolbar.addEventListener('click', e => {
+      if (e.target.id === 'clear') {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+  });
+
+  toolbar.addEventListener('change', e => {
+      if(e.target.id === 'stroke') {
+          ctx.strokeStyle = e.target.value;
+      }
+
+      if(e.target.id === 'lineWidth') {
+          lineWidth = e.target.value;
+      }
+      
+  });
+
+  const draw = (e) => {
+      if(!isPainting) {
+          return;
+      }
+
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = 'round';
+
+      ctx.lineTo(e.clientX - (canvasOffsetX/2.05), e.clientY - canvasOffsetY);
+      ctx.stroke();
+  }
+
+  canvas.addEventListener('mousedown', (e) => {
+      isPainting = true;
+      startX = e.clientX;
+      startY = e.clientY;
+  });
+
+  canvas.addEventListener('mouseup', e => {
+      isPainting = false;
+      ctx.stroke();
+      ctx.beginPath();
+  });
+
+  canvas.addEventListener('mousemove', draw);
+}
 
 function clearError() {
   app.error = null;
